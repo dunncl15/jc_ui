848101f92ecccc15d6a728353f05bf17b271d264
Charles Dunn <dunnc1551@gmail.com>
2019-08-16 16:30:39 -0600

    Add form route to create first user.

--- src/components/Users/UsersLanding/UsersLanding.js
+++ src/components/Users/UsersLanding/UsersLanding.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import NewUserForm from '../NewUserForm/NewUserForm';
 import background from '../../../images/usersGettingStarted.svg';
 import './UsersLanding.scss';
 
-const UsersLanding = ({ history }) => {
+const UsersLanding = ({ history, users, saveNewUser }) => {
   return (
     <section className="users--landing-page">
       <h2>Welcome to JumpCloud!</h2>
       <img className="default-img" src={background} alt="" />
       <p>
-        Get started by adding users. Then give them secure access to any
-        resource, including systems (Mac, Windows, Linux), directories (LDAP,
-        GSuite), SSO applications, RADIUS networks (WiFi, VPN), and more.
+        Get started by adding users. Then give them secure access to any resource,
+        including systems (Mac, Windows, Linux), directories (LDAP, GSuite), SSO
+        applications, RADIUS networks (WiFi, VPN), and more.
       </p>
       <button className="cta-btn" onClick={() => history.push('/users/add')}>
         add your first user
@@ -23,6 +25,13 @@ const UsersLanding = ({ history }) => {
       >
         learn more
       </a>
+      <Route
+        exact
+        path="/users/add"
+        render={() => (
+          <NewUserForm history={history} users={users} saveNewUser={saveNewUser} />
+        )}
+      />
     </section>
   );
 };
