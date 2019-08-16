19ea5bbb331a71cd03138490707c64b39ab4e1d3
Charles Dunn <dunnc1551@gmail.com>
2019-08-15 22:51:14 -0600

    Add post and put functionality.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
 import UsersLanding from './UsersLanding/UsersLanding';
 import SearchBar from './SearchBar/SearchBar';
 import UserList from './UsersList/UserList';
+import NewUserForm from './NewUserForm/NewUserForm';
 
 const Users = ({ history }) => {
   const [users, saveUsers] = useState([]);
@@ -21,23 +23,45 @@ const Users = ({ history }) => {
   };
 
   const selectUser = (e, id) => {
-    const users = e.target.checked
-      ? selected.concat(id)
-      : selected.filter(v => v !== id);
+    const users = e.target.checked ? selected.concat(id) : selected.filter(v => v !== id);
     setSelected(users);
   };
 
+  const updateUser = user => {
+    const updated = users.map(obj => (obj.id === user.id ? { ...obj, ...user } : obj));
+    saveUsers(updated);
+  };
+
+  const saveNewUser = user => saveUsers(users.concat(user));
+
   if (error) return <span>{error}</span>;
   if (!users.length) return <UsersLanding history={history} />;
-
+  console.log('USERS: ', users);
   return (
     <>
       <SearchBar history={history} selected={selected} count={users.length} />
       <UserList
+        history={history}
         users={users}
         selectUser={selectUser}
         selectAll={selectAll}
         selected={selected}
+      />
+      <Route
+        path="/users/:id/details"
+        render={({ match }) => (
+          <NewUserForm
+            history={history}
+            data={users.find(user => user.id === match.params.id)}
+            id={match.params.id}
+            updateUser={updateUser}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/users/add"
+        render={() => <NewUserForm users={users} saveNewUser={saveNewUser} />}
       />
     </>
   );
