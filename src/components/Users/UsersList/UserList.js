62b67b1ace8f480bbbc8f2a51daab409bd981dc5
Charles Dunn <dunnc1551@gmail.com>
2019-08-15 23:03:34 -0600

    Fix click on checkbox vs parent el.

--- src/components/Users/UsersList/UserList.js
+++ src/components/Users/UsersList/UserList.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './UserList.scss';
 
-const UserList = ({ users, selectUser, selectAll, selected }) => {
+const UserList = ({ history, users, selectUser, selectAll, selected }) => {
+  const handleClick = (e, id) => {
+    if (e.target.id !== id) history.push(`/users/${id}/details`);
+  };
   return (
     <div className="user-list">
       <div className="header">
@@ -21,29 +24,33 @@ const UserList = ({ users, selectUser, selectAll, selected }) => {
         {users.map(user => {
           const checked = selected.indexOf(user.id) > -1;
           return (
-            <li key={user.id} className={checked ? 'user selected' : 'user'}>
+            <li
+              key={user.id}
+              className={checked ? 'user selected' : 'user'}
+              onClick={e => handleClick(e, user.id)}
+            >
               <input
+                id={user.id}
                 type="checkbox"
                 checked={checked}
                 onChange={e => selectUser(e, user.id)}
               />
               <span className="col col-xs">
-                <i className="material-icons">
-                  {user.activated ? null : 'more_horiz'}
+                <i
+                  className="material-icons"
+                  style={{ backgroundColor: user.activated ? '#2cc692' : '#ffb000' }}
+                >
+                  {user.activated ? 'check' : 'more_horiz'}
                 </i>
               </span>
               <div className="col col-s vertical">
-                <span className="primary">{`${user.firstname} ${
-                  user.lastname
-                }`}</span>
+                <span className="primary">{`${user.firstname} ${user.lastname}`}</span>
                 <span className="secondary">{user.username}</span>
               </div>
               <span className="col col-s">{user.email}</span>
               <span className="col col-l">{user.mfa.configured}</span>
               <span className="col col-l">
-                {user.password_expired || (
-                  <i className="material-icons">remove</i>
-                )}
+                {user.password_expired || <i className="material-icons">remove</i>}
               </span>
               <Link to={`/users/${user.id}/details`}>
                 <i className="material-icons">keyboard_arrow_right</i>
