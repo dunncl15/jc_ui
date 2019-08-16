6c3fdb54767081d835e2e46a6fd0252d5018e319
Charles Dunn <dunnc1551@gmail.com>
2019-08-15 18:22:30 -0600

    Add select and select all functionality.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,31 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import background from '../../images/usersGettingStarted.svg';
-import './Users.scss';
+import UsersLanding from './UsersLanding/UsersLanding';
+import SearchBar from './SearchBar/SearchBar';
+import UserList from './UsersList/UserList';
 
-const Users = () => {
+const Users = ({ history }) => {
   const [users, saveUsers] = useState([]);
-  const [count, saveCount] = useState(0);
   const [error, setError] = useState(null);
+  const [selected, setSelected] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8005/api/systemusers')
       .then(res => res.json())
-      .then(({ totalCount, results }) => {
-        saveUsers(results);
-        saveCount(totalCount);
-      })
-      .catch(e => console.log('err: ', e));
+      .then(({ results }) => saveUsers(results))
+      .catch(e => setError(e));
   }, []);
+
+  const selectAll = e => {
+    const selected = e.target.checked ? users.map(u => u.id) : [];
+    setSelected(selected);
+  };
+
+  const selectUser = (e, id) => {
+    const users = e.target.checked
+      ? selected.concat(id)
+      : selected.filter(v => v !== id);
+    setSelected(users);
+  };
+
+  if (error) return <span>{error}</span>;
+  if (!users.length) return <UsersLanding history={history} />;
+
   return (
-    <section className="users--landing-page">
-      <h3>Welcome to JumpCloud!</h3>
-      <img className="default-img" src={background} alt="" />
-      <p>
-        Get started by adding users. Then give them secure access to any
-        resource, including systems (Mac, Windows, Linux), directories (LDAP,
-        GSuite), SSO applications, RADIUS networks (WiFi, VPN), and more.
-      </p>
-    </section>
+    <>
+      <SearchBar history={history} selected={selected} count={users.length} />
+      <UserList
+        users={users}
+        selectUser={selectUser}
+        selectAll={selectAll}
+        selected={selected}
+      />
+    </>
   );
 };
 
