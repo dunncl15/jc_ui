6509ffb71179fde644c9866d5641bd7dd7f2e8c6
Charles Dunn <dunnc1551@gmail.com>
2019-08-15 18:18:28 -0600

    Add email and icon to header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,8 @@ const Header = ({ location }) => {
   return (
     <header id="header">
       <h1>{location.pathname.slice(1)}</h1>
+      <span>dunnc1551@gmail.com</span>
+      <i className="material-icons">notifications</i>
     </header>
   );
 };
