720d2e90b29f081cd9476aca57086910da896fc4
Charles Dunn <dunnc1551@gmail.com>
2019-08-16 16:31:23 -0600

    Fix header to render only applicable path name.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Header.scss';
 
 const Header = ({ location }) => {
+  const [x, path] = location.pathname.split('/');
   return (
     <header id="header">
-      <h1>{location.pathname.slice(1)}</h1>
+      <h1>{path}</h1>
       <span>dunnc1551@gmail.com</span>
       <i className="material-icons">notifications</i>
     </header>
