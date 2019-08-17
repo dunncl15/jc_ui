619eb9721e98d260aadb3291aa9c763d4bc8efa1
Charles Dunn <dunnc1551@gmail.com>
2019-08-16 18:37:12 -0600

    Implement delete and refactor initial get request.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
+import { get, del } from '../../api/api_client';
 import UsersLanding from './UsersLanding/UsersLanding';
 import SearchBar from './SearchBar/SearchBar';
 import UserList from './UsersList/UserList';
 import NewUserForm from './NewUserForm/NewUserForm';
+import Error from '../Error/Error';
 
 const Users = ({ history }) => {
   const [users, saveUsers] = useState([]);
@@ -11,10 +13,9 @@ const Users = ({ history }) => {
   const [selected, setSelected] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:8005/api/systemusers')
-      .then(res => res.json())
+    get()
       .then(({ results }) => saveUsers(results))
-      .catch(e => setError(e));
+      .catch(e => setError(e.text));
   }, []);
 
   const selectAll = e => {
@@ -34,12 +35,36 @@ const Users = ({ history }) => {
 
   const saveNewUser = user => saveUsers(users.concat(user));
 
-  if (error) return <span>{error}</span>;
-  if (!users.length) return <UsersLanding history={history} />;
+  const deleteHandler = async () => {
+    try {
+      const requests = selected.map(id => del(id));
+      const responses = await Promise.all(requests);
+      const ids = responses.reduce((acc, data) => {
+        acc[data.id] = data.id;
+        return acc;
+      }, {});
+      const remainingUsers = users.filter(user => typeof ids[user.id] !== 'string');
+      saveUsers(remainingUsers);
+      setSelected([]);
+    } catch (e) {
+      setError(e.text);
+    }
+  };
+
+  if (error) return <Error text={error} setError={setError} />;
+  if (!users.length)
+    return (
+      <UsersLanding history={history} saveNewUser={saveNewUser} setError={setError} />
+    );
 
   return (
     <>
-      <SearchBar history={history} selected={selected} count={users.length} />
+      <SearchBar
+        history={history}
+        selected={selected}
+        count={users.length}
+        deleteUser={deleteHandler}
+      />
       <UserList
         history={history}
         users={users}
@@ -55,13 +80,16 @@ const Users = ({ history }) => {
             data={users.find(user => user.id === match.params.id)}
             id={match.params.id}
             updateUser={updateUser}
+            setError={setError}
           />
         )}
       />
       <Route
         exact
         path="/users/add"
-        render={() => <NewUserForm users={users} saveNewUser={saveNewUser} />}
+        render={() => (
+          <NewUserForm history={history} saveNewUser={saveNewUser} setError={setError} />
+        )}
       />
     </>
   );
